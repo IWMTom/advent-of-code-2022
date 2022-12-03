7eb82484ac9f48d74637d9a13b9e975b9af2c321
Tom Wilson <tom@iwm.pw>
2022-12-03 16:36:27 +0000

    Day 3

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { format } = require("date-fns");
 const { dayOne } = require("./days/01/solution");
 const { dayTwo } = require("./days/02/solution");
+const { dayThree } = require("./days/03/solution");
 require("better-logging")(console, {
   format: (ctx) => `${ctx.STAMP(format(new Date(), "dd/MM/yyyy HH:mm:ss.SSS"))} ${ctx.type} ${ctx.msg}`,
 });
 
-const DAY_TO_RUN = 2;
+const DAY_TO_RUN = 3;
 const main = async () => {
   console.log("Advent of Code 2022");
   console.log(`Running solution for day ${DAY_TO_RUN}`);
@@ -16,6 +17,9 @@ const main = async () => {
       break;
     case 2:
       await dayTwo();
+      break;
+    case 3:
+      await dayThree();
       break;
   }
 };
